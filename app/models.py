9fd2e9d9259e69c3421593b107b42e7951def372
angelicty <muyenziraissa@gmail.com>
2019-09-27 14:51:59 +0200

    Adding writer class

--- app/models.py
+++ app/models.py
@@ -10,7 +10,34 @@ def load_writer(writer_id):
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     posts = db.relationship('Post',backref = 'user',lazy = "dynamic")
     comments = db.relationship('Comment',backref = 'user',lazy="dynamic") 
+
+class Writer(WriterMixin,db.Model):
+    __tablename__ = 'writters'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+   
+    password_secure = db.Column(db.String(255))
+
+    pass_secure = db.Column(db.String(255))
+    posts = db.relationship('Post',backref = 'writer',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'writer',lazy = "dynamic")
+   
+
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password attribute')
+
     
+    @password.setter 
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    def __repr__(self):
+        return f'Writer {self.username}'
+
